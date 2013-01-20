8a1b8459fd4fa5d6f27522aa01cd4228d5191702
l0ner <pwslts@gmail.com>
2013-01-20 14:13:17 +0100

    layout 12: removed picasa link replacer

--- js/rndstr.js
+++ js/rndstr.js
@@ -19,21 +19,5 @@ $('document').ready(function(){
 	setInterval(function(){
 			$('#randomCh').html(randString(29-section.length));
     }, 50);
-	
-	// picasa link replacer
-	// disbled for compatibility reasons
-	$('.picasaPhoto').brscPicasa({
-		photos_thumbsize: '64c',
-		photo_displaysize: '600',
-		classes: {
-			link_with_image: 'loaded-link'
-		},
-		callback: function() {
-			$('a.loaded-link img', this).parent().click(function() {
-				$(this).slimbox();
-			});
-		}
-	});
-
 });
 
